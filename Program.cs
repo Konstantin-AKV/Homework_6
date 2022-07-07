c1c7d2db790811946cd3aaf562fa24dfbd2844d5
Konstantin-AKV <abdulaev-kostya@mail.ru>
2022-07-08 01:21:00 +0300

    Задача 41

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine($"Введи числа с клавиатуры через запятую: ");
+string s = Console.ReadLine();
+string[] M = s.Split(',');
+int[] mass = new int[M.Length];
+int count = 0;
+for (int i = 0; i < M.Length; i++)
+{
+    mass[i] = int.Parse(M[i]);
+
+if (mass[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Количество чисел больше ноля: " + count);
